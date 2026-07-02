2ec8f310d4d1c9faa84d489c06359e2c37117a8d
safuwhale <nandomdrimz@gmail.com>
2026-07-02 13:40:45 +0100

    dashboard upgrade

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 # We will update this URL shortly
 SQLALCHEMY_DATABASE_URL = "postgresql://church_admin:admin123@localhost/church_db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
